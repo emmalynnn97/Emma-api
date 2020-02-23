e1440b9119306163d6633daf28c0a29bd3f96bd7
emmalynnn97 <emmalynnn97@gmail.com>
2020-02-23 13:59:36 -0700

    cleaning up comments

--- server.js
+++ server.js
@@ -5,36 +5,37 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
 
+//Import the various types of routes
 const projectsRoute = require('./routes/projects')
 const pagesRoute = require('./routes/pages')
 const menusRoute = require('./routes/menus');
 
-//Set up cross origin resource sharing, enable the body parser
+//Enable up cross origin resource sharing, enable the body parser
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//The port for the server to listen on
+//Set the port for the server to listen on
 const port = process.env.PORT || 5000;
+
 //connect to DB
-mongoose.connect(process.env.DB_CONNECTION, {useUnifiedTopology:true,useNewUrlParser:true} ,()=>{
+mongoose.connect(process.env.DB_CONNECTION, 
+    {useUnifiedTopology:true,useNewUrlParser:true} ,
+    ()=>{
     console.log('Connected to DB');
 });
 
-
-
+//Middleware to enable the various types of routes
 app.use('/pages',pagesRoute);
 app.use('/projects',projectsRoute);
 app.use('/menus',menusRoute);
+
 //The Root route
 app.get('/', (req, res) => [
-    res.status(200).send('API Root')
+    res.status(200).send('API Root Route, try using /projects, /pages, or /menus to get started')
 ]);
-
 
 //Start the server, listen on port specified in the port const
 app.listen(port, () => {
     console.log(`API is listening on port ${port}`)
 });
-
-
